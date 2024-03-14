e1865f6c6fe0db01b679d06598f39e61d4d7ab05
Sirachet Chotthakunanan <sirachet.c@gmail.com>
2024-03-14 23:15:19 +0700

    Update Lab1203.py

--- Lab12/Lab1203.py
+++ Lab12/Lab1203.py
@@ -5,25 +5,42 @@ class City:
         '''init'''
         self.name = name
         self.cities = cities
-
+ 
     def get_name(self):
+        '''getname'''
         return self.name
-
-    def get_cities(self):
+ 
+    def get_city(self):
+        '''getcity'''
         return self.cities
-
+ 
 import json
-def findStations(allstations, numstation):
+def findstations(allstations, numstation):
     '''findStations'''
-    stationlist = []
+    stationlist, newlist, staion, num = [], [], set(), 0
+ 
     for _ in range(numstation):
         stain = json.loads(input())
         stationlist.append(
             City(stain['Name'], stain['Cities'])
         )
-    stationlist.sort(key=lambda x: len(x.get_cities()), reverse=True)
-    
-    # for i in stationlist:
-    #     print(i.get_name(), i.get_cities())
-
-findStations(json.loads(input()), int(input()))
+    stationlist.sort(key=lambda x: len(x.get_city()), reverse=True)
+    for j in stationlist:
+        if len(newlist) == len(allstations):
+            break
+        for i in range((len(stationlist[num].get_city()))):
+            word = j.get_city()[i]
+            if word in allstations:
+                if word in newlist:
+                    continue
+                else:
+                    newlist.append(word)
+                    staion.add(j.get_name())
+                    for new in range(num+1, len(stationlist)):
+                        if word in stationlist[new].get_city():
+                            stationlist[new].get_city().remove(word)
+                    stationlist.sort(key=lambda x: len(x.get_city()), reverse=True)
+        num += 1
+    staion = list(staion)
+    print(sorted(staion))
+findstations(json.loads(input()), int(input()))
